38eddaea7ea3a1cdee136f51e0a5df3779ad43ea
Axel Santadi <axelsantadi@gmail.com>
2024-04-03 16:27:40 +0700

    added keyboard.c & keyboard.h

--- makefile
+++ makefile
@@ -32,6 +32,7 @@ kernel:
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/stdlib/string.c -o $(OUTPUT_FOLDER)/string.o
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/interrupt.c -o $(OUTPUT_FOLDER)/interrupt.o
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/idt.c -o $(OUTPUT_FOLDER)/idt.o
+	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/keyboard.c -o $(OUTPUT_FOLDER)/keyboard.o
 	@$(ASM) $(AFLAGS) $(SOURCE_FOLDER)/header/cpu/intsetup.s -o $(OUTPUT_FOLDER)/intsetup.o
 	@$(LIN) $(LFLAGS) bin/*.o -o $(OUTPUT_FOLDER)/kernel
 	@echo Linking object files and generate elf32...
---
+++ src/keyboard.c
@@ -0,0 +1,60 @@
+#include "header/driver/keyboard.h"
+#include "header/text/framebuffer.h"
+#include "header/cpu/portio.h"
+#include "header/stdlib/string.h"
+
+static int row = 0;
+//static int col = 0;
+
+const char keyboard_scancode_1_to_ascii_map[256] = {
+      0, 0x1B, '1', '2', '3', '4', '5', '6',  '7', '8', '9',  '0',  '-', '=', '\b', '\t',
+    'q',  'w', 'e', 'r', 't', 'y', 'u', 'i',  'o', 'p', '[',  ']', '\n',   0,  'a',  's',
+    'd',  'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',   0, '\\',  'z', 'x',  'c',  'v',
+    'b',  'n', 'm', ',', '.', '/',   0, '*',    0, ' ',   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0, '-',    0,    0,   0,  '+',    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+};
+
+struct KeyboardDriverState keyboard_state = {false, false, '\0'};
+
+void keyboard_state_activate(void) {
+    keyboard_state.keyboard_input_on = true;
+    // Enable keyboard interrupt here (implementation dependent)
+}
+
+void keyboard_state_deactivate(void) {
+    keyboard_state.keyboard_input_on = false;
+    // Disable keyboard interrupt here (implementation dependent)
+}
+
+void get_keyboard_buffer(char *buf) {
+    *buf = keyboard_state.keyboard_buffer;
+    keyboard_state.keyboard_buffer = '\0';
+}
+
+void keyboard_isr(void) {
+    // Read the scancode from the keyboard port
+    uint8_t scancode = in(KEYBOARD_DATA_PORT);
+
+    char ascii_char = keyboard_scancode_1_to_ascii_map[scancode];
+    if (ascii_char != 0 && ascii_char != '\n') {
+        // Print the ASCII character to the screen
+        keyboard_state.keyboard_buffer = keyboard_scancode_1_to_ascii_map[scancode];
+    } else if (ascii_char == '\n') {
+        framebuffer_set_cursor(row++, 0);
+    }
+
+    // End of interrupt signal
+    out(0x20, 0x20);
+}
