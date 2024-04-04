ca14c11925b6a7bee9d3052050a99d4dd24ddcc2
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2024-04-05 04:52:39 +0700

    UPDATE 1.4

--- makefile
+++ makefile
@@ -15,13 +15,21 @@ STRIP_CFLAG   = -nostdlib -fno-stack-protector -nostartfiles -nodefaultlibs -ffr
 CFLAGS        = $(DEBUG_CFLAG) $(WARNING_CFLAG) $(STRIP_CFLAG) -m32 -c -I$(SOURCE_FOLDER)
 AFLAGS        = -f elf32 -g -F dwarf
 LFLAGS        = -T $(SOURCE_FOLDER)/linker.ld -melf_i386
+DISK_NAME     = storage
+
+disk:
+	@qemu-img create -f raw $(OUTPUT_FOLDER)/$(DISK_NAME).bin 4M
+
+
+
 
 run: all
-	@qemu-system-i386 -s -S -cdrom $(OUTPUT_FOLDER)/$(ISO_NAME).iso
+	@qemu-system-i386 -s -S -drive file=sample-image.bin,format=raw,if=ide,index=0,media=disk -cdrom OS2024.iso
+
 all: build
 build: iso
 clean:
-	rm -rf *.o *.iso $(OUTPUT_FOLDER)/kernel
+	rm -rf *.o *.iso $(OUTPUT_FOLDER)/kernel $(OUTPUT_FOLDER)/*o $(OUTPUT_FOLDER)/*.iso $(OUTPUT_FOLDER)/*.bin
 
 kernel:
 	@$(ASM) $(AFLAGS) $(SOURCE_FOLDER)/kernel-entrypoint.s -o $(OUTPUT_FOLDER)/kernel-entrypoint.o
@@ -32,6 +40,8 @@ kernel:
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/stdlib/string.c -o $(OUTPUT_FOLDER)/string.o
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/interrupt.c -o $(OUTPUT_FOLDER)/interrupt.o
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/idt.c -o $(OUTPUT_FOLDER)/idt.o
+	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/disk.c -o $(OUTPUT_FOLDER)/disk.o
+	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/fat32.c -o $(OUTPUT_FOLDER)/fat32.o
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/keyboard.c -o $(OUTPUT_FOLDER)/keyboard.o
 	@$(ASM) $(AFLAGS) $(SOURCE_FOLDER)/header/cpu/intsetup.s -o $(OUTPUT_FOLDER)/intsetup.o
 	@$(LIN) $(LFLAGS) bin/*.o -o $(OUTPUT_FOLDER)/kernel
--- src/kernel.c
+++ src/kernel.c
@@ -5,20 +5,87 @@
 #include "header/cpu/idt.h"
 #include "header/cpu/interrupt.h"
 #include "header/driver/keyboard.h"
+#include "header/driver/disk.h"
+#include "header/filesystem/fat32.h"
+#include "header/stdlib/string.h"
 
-void kernel_setup(void) {
+// void kernel_setup(void) {
+//     load_gdt(&_gdt_gdtr);
+//     pic_remap();
+//     initialize_idt();
+//     activate_keyboard_interrupt();
+//     framebuffer_clear();
+//     framebuffer_set_cursor(0, 0);
+
+//     //int col = 0;
+//     keyboard_state_activate();
+//     while (true) {
+//         char c;
+//         get_keyboard_buffer(&c);
+//         //if (c) framebuffer_write(0, col++, c, 0xF, 0);
+//     }
+// }
+
+// void kernel_setup(void) {
+//     load_gdt(&_gdt_gdtr);
+//     pic_remap();
+//     activate_keyboard_interrupt();
+//     initialize_idt();
+//     framebuffer_clear();
+//     framebuffer_set_cursor(0, 0);
+
+//     write_blocks(fs_signature, 0, 1);
+
+//     struct BlockBuffer b;
+//     for (int i = 0; i < 512; i++) b.buf[i] = i % 16;
+//     write_blocks(&b, 17, 1);
+//     while (true);
+// }
+
+/* CRUD KERNEL */
+
+void kernel_setup(void)
+{
     load_gdt(&_gdt_gdtr);
     pic_remap();
     initialize_idt();
     activate_keyboard_interrupt();
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
-        
-    int col = 0;
-    keyboard_state_activate();
-    while (true) {
-        char c;
-        get_keyboard_buffer(&c);
-        if (c) framebuffer_write(0, col++, c, 0xF, 0);
+    initialize_filesystem_fat32();
+
+    struct ClusterBuffer cbuf[5];
+    for (uint32_t i = 0; i < 5; i++)
+        for (uint32_t j = 0; j < CLUSTER_SIZE; j++)
+            cbuf[i].buf[j] = i + 'a';
+
+    struct FAT32DriverRequest request = {
+        .buf = cbuf,
+        .name = "root",
+        .ext = "uwu",
+        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+        .buffer_size = 0,
+    };
+
+    write(request); 
+    memcpy(request.name, "folder1\0\0\0", 8);
+    write(request);
+
+    memcpy(request.name, "daijobu", 8);
+    memcpy(request.ext, "\0\0\0", 3);
+    write(request); 
+
+    memcpy(request.name, "folder2", 8);
+    write(request); 
+
+    memcpy(request.name, "kano\0\0\0\0\0", 8);
+    write(request); 
+
+    memcpy(request.name, "nbuba", 8);
+    write(request); 
+    // delete (request);
+    while (true)
+    {
+        keyboard_state_activate();
     }
 }
